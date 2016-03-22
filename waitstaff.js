a4717fdf5f3fb3b8e7e6187e8a494883bfe88b34
Capi Etheriel <barraponto@gmail.com>
2016-03-22 19:45:13 -0300

    using the Meal object

--- waitstaff.js
+++ waitstaff.js
@@ -1,15 +1,4 @@
 angular.module('Waitstaff', [])
 .controller('WaitstaffCtrl', function($scope){
-    $scope.subtotal = function(){
-        var mp = $scope.mealprice||0;
-        var tr = $scope.taxrate||0;
-        return mp * (1+tr/100);
-    };
-    $scope.tip = function(){
-        var tp = $scope.tiprate||0;
-        return (tp/100)*$scope.subtotal();
-    };
-    $scope.total = function(){
-        return $scope.subtotal() + $scope.tip();
-    };
+    $scope.meal = new Meal();
 });
