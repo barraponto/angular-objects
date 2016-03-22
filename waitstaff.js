0e6ab5d521267b142aac0a6bcb6e3aedcc8a4dc7
Capi Etheriel <barraponto@gmail.com>
2016-03-22 17:54:15 -0300

    setup customer charges functions in the controller

--- waitstaff.js
+++ waitstaff.js
@@ -1,4 +1,15 @@
 angular.module('Waitstaff', [])
 .controller('WaitstaffCtrl', function($scope){
-
+    $scope.subtotal = function(){
+        var mp = $scope.mealprice||0;
+        var tr = $scope.taxrate||0;
+        return mp * (1+tr/100);
+    };
+    $scope.tip = function(){
+        var tp = $scope.tiprate||0;
+        return (tp/100)*$scope.subtotal();
+    };
+    $scope.total = function(){
+        return $scope.subtotal() + $scope.tip();
+    };
 });
