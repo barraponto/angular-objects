47d48c60d109148ba66243c86cb19e5c92f454ac
Capi Etheriel <barraponto@gmail.com>
2016-03-22 22:21:35 -0300

    define a waiter object

--- waitstaff.js
+++ waitstaff.js
@@ -1,4 +1,10 @@
 angular.module('Waitstaff', [])
 .controller('WaitstaffCtrl', function($scope){
+    $scope.waiter = new Waiter();
     $scope.meal = new Meal();
+
+    $scope.onSubmit = function(e) {
+        $scope.waiter.addTip($scope.meal.tip());
+        $scope.meal = new Meal();
+    };
 });
